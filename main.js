5141e8198977d09aaa3c43727e8affc3e78501c8
graceK-10 <kipagrace@gmail.com>
2024-04-28 11:24:53 +0200

    cart changes

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function displayMessage() {
-    alert("Thank You for shopping with Skincare");
-}
+function addToCart() {
+    let productName = "La Roche Posay"; // Replace this with the actual product name
+    let quantity = document.getElementById("quantity").value;
+    let totalPrice = quantity * 650; // Assuming each product costs $10, you can replace this with your actual pricing logic
+    let cartContents = productName + " x " + quantity + " = R" + totalPrice;
+
+    // Display the cart contents
+    var cartElement = document.getElementById("cart");
+    cartElement.innerHTML = cartContents;
+  }
